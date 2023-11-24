59bf43ea46b4888670c119af43be7717c1051b5b
Ramon <ramondavi@msn.com>
2023-11-23 23:27:01 -0300

    Focus Time

---
+++ src/features/FocusHistory.js
@@ -0,0 +1,39 @@
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/sizes';
+
+const FocusHistory = ({ history }) => {
+
+if(!history || !history.length) return <Text style={styles.title}>We haven't focused on anything yet!</Text>;
+
+const renderItem = ({ item }) => <Text style={styles.item}>- {item}</Text>
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we've focused on: </Text>
+      <FlatList
+        data={history}
+        renderItem={renderItem}
+        />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.md,
+    flex: 1
+  },
+  item: {
+    fontSize: fontSizes.md,
+    color: colors.white,
+    paddingTop: spacing.md
+  }, 
+  title: {
+    color: colors.white,
+    fontSize: fontSizes.lg,
+    fontWeight: 'bold'
+  }
+})
+
+export default FocusHistory;
